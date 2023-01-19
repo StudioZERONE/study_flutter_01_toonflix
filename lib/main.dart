6b73964c33ff456028ee718f1af9ccb8b63111f6
ZERONE <zerone012@gmail.com>
2023-01-19 10:06:38 +0900

    UI 개발 중. Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,52 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: const Color(0xFFF4EDDB),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Click Counter',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              '$counter',
+              style: const TextStyle(fontSize: 30),
+            ),
+            IconButton(
+              iconSize: 40,
+              onPressed: onClicked,
+              icon: const Icon(
+                Icons.add_box_rounded,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  @override
+  Widget build_(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           backgroundColor: const Color(0xFF181818),
